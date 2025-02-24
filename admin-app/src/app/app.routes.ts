d7cc0877968244fa6feac2db1fa5235f9366bb24
BlancaSC13 <blanca.segura@ucr.ac.cr>
2025-02-23 22:52:42 -0600

    AA-B1 Professor Register Completo

--- admin-app/src/app/app.routes.ts
+++ admin-app/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RegistrationApplicationsComponent } from './components/registration-app
 import { NewsComponent } from './components/news/news.component';
 import { NewsDetailsComponent } from './components/news-details/news-details.component';
 import { CreateNewsComponent } from './components/create-news/create-news.component';
+import { RegisterProfessorComponent } from './components/register-professor/register-professor.component';
 
 export const routes: Routes = [
-    {path: 'applications', component: RegistrationApplicationsComponent},
-    {path: 'news', component: NewsComponent},
-    {path: 'createNews', component: CreateNewsComponent},
-    {path: 'news/:id', component: NewsDetailsComponent},
-    {path: '', component: NewsComponent},
+  { path: 'applications', component: RegistrationApplicationsComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'createNews', component: CreateNewsComponent },
+  { path: 'news/:id', component: NewsDetailsComponent },
+  { path: 'registerProfessor', component: RegisterProfessorComponent },
+  { path: '', component: NewsComponent },
 ];
