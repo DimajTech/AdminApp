e7b7da4b9d7edc40d5cec4c4f3d6aa5cc4679d06
AxelSV50 <axelsv50@gmail.com>
2025-02-25 04:41:10 -0600

    Se elimina el prerending

--- admin-app/src/app/app.config.server.ts
+++ admin-app/src/app/app.config.server.ts
@@ -1,11 +1,8 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRoutesConfig } from '@angular/ssr';
 import { appConfig } from './app.config';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(),
   ]
 };
 
