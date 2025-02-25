565d815a9c4b459cec4ce027ce8957994a9357f2
AxelSV50 <axelsv50@gmail.com>
2025-02-25 05:34:50 -0600

    Corrección Toast

--- admin-app/src/app/components/register-professor/register-professor.component.ts
+++ admin-app/src/app/components/register-professor/register-professor.component.ts
@@ -4,9 +4,9 @@ import { v4 as uuidv4 } from 'uuid';
 import { RegisterProfessorService } from '../../services/register-professor.service';
 import { FormsModule, NgForm } from '@angular/forms';
 import { NgIf } from '@angular/common';
-import Toastify from 'toastify-js';
+/*import Toastify from 'toastify-js';
 import 'toastify-js/src/toastify.css';
-
+*/
 @Component({
   selector: 'app-register-professor',
   imports: [FormsModule, NgIf],
@@ -26,12 +26,12 @@ export class RegisterProfessorComponent {
 
   submitProfessor(professorForm: NgForm): void {
     if (!this.name || !this.email || !this.password || !this.confirmPassword) {
-      this.showToast('Todos los campos son obligatorios.', 'error');
+      //this.showToast('Todos los campos son obligatorios.', 'error');
       return;
     }
 
     if (this.password !== this.confirmPassword) {
-      this.showToast('Las contraseñas no coinciden.', 'error');
+      //this.showToast('Las contraseñas no coinciden.', 'error');
       return;
     }
 
@@ -45,7 +45,7 @@ export class RegisterProfessorComponent {
 
     this.service.registerProfessor(professorData).subscribe({
       next: () => {
-        this.showToast('Usuario registrado con éxito.', 'success');
+        //this.showToast('Usuario registrado con éxito.', 'success');
 
         //Envío de correo para informe de registro
         this.service.sendRegistrationEmail(professorData).subscribe({
@@ -54,7 +54,7 @@ export class RegisterProfessorComponent {
           },
           error: (err: any) => {
             console.error('Error al enviar el correo:', err);
-            this.showToast('Error al enviar el correo.', 'error');
+            //this.showToast('Error al enviar el correo.', 'error');
           },
         });
 
@@ -62,32 +62,34 @@ export class RegisterProfessorComponent {
         this.service.registerOnStudent(professorData).subscribe({
           next: () => {},
           error: (err: any) => {
-            this.showToast('Error al registrar en StudentApp.', 'error');
+           //this.showToast('Error al registrar en StudentApp.', 'error');
           },
         });
         //Registro en ProfessorApp
         this.service.registerOnProfessor(professorData).subscribe({
           next: () => {
-            this.showToast(
+            /*this.showToast(
               'Usuario registrado con éxito en Professor.',
               'success'
             );
+            */
           },
           error: (err: any) => {
-            this.showToast('Error al registrar en Professor.', 'error');
+           // this.showToast('Error al registrar en Professor.', 'error');
           },
         });
         professorForm.reset();
       },
       error: (err: any) => {
-        this.showToast(
+        /*this.showToast(
           err.error.message || 'Error al registrar el usuario.',
           'error'
-        );
+        );*/
       },
     });
   }
 
+  /*
   showToast(message: string, type: 'success' | 'error') {
     Toastify({
       text: message,
@@ -98,6 +100,7 @@ export class RegisterProfessorComponent {
       backgroundColor: type === 'success' ? 'green' : 'red',
     }).showToast();
   }
+    */
   resetForm(professorForm: NgForm): void {
     professorForm.reset();
   }
